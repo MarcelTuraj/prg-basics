7d7e81a83363f4783327f2c729d4063d8c78c91e
Marcel Turaj <meny8111@gmail.com>
2024-10-10 00:10:16 +0200

    s3 almost finished + breakdown

--- 02-TypesAndVariables/s3x6.py
+++ 02-TypesAndVariables/s3x6.py
@@ -2,7 +2,7 @@
 #Distance to the horizon - calculator
 #
 import math
-height = input("Enter your height in meters")
-height = int
+height = input("Enter your height in meters ")
+height = float(height)
 distance = 3.57 * math.sqrt(height)
 print(distance)
