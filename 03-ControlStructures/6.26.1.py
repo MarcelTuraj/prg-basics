7c693e8993a32f15ea1956f42e154f33140f1698
Marcel Turaj <meny8111@gmail.com>
2024-10-21 16:20:48 +0200

    5left.

--- 03-ControlStructures/6.26.1.py
+++ 03-ControlStructures/6.26.1.py
@@ -1,12 +1,14 @@
 pin = "0805"
-user1 = 0
-for i in range(1,3) :
+for i in range(1,4) :
     user = str(input("enter pin: "))
     if user == pin: 
         print("correct")
         break
     elif user != pin:
         continue
+    
+if user != pin :
+    print("Account blocked. ")
    
 
 
