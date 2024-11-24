7cdedb0ade43088ea3e4ec6d71a44e878163ccf3
Marcel Turaj <meny8111@gmail.com>
2024-11-24 17:50:56 +0100

    matrixxxx

--- 07-Arrays/3.35.py
+++ 07-Arrays/3.35.py
@@ -1,7 +1,8 @@
 matrix = [
     [1,2,3],
     [4,5,6],
-    [7,8,9]
+    [7,8,9],
+    [10,12,13,14]
 
 ]
 
@@ -9,7 +10,7 @@ def transposition(m):
    transposed = [[0]*len(matrix) for i in range(len(matrix[0]))]
    for i in range(len(matrix)):
       for k in range(len(matrix[0])):
-         transposed[i][k] = m[k][i]
+         transposed[k][i] = m[i][k]
    return transposed
 
 array = [[0]*4 for i in range(9)]
